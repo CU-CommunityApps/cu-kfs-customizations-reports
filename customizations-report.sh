2845f795b6882fbafe6553906a43875a6be25426
Jay Hulslander <jdh34@cornell.edu>
2021-07-26 10:44:46 -0400

    clean up

--- customizations-report.sh
+++ customizations-report.sh
@@ -2,40 +2,111 @@ git clone https://github.com/CU-CommunityApps/cu-kfs
 git clone https://github.com/kualico/financials
 cd cu-kfs
 
-K_J_SYS=("KualiCo base overlay SYS" "src/main/java/org/kuali/kfs/sys" "../financials/kfs-core/" "false" "true")
-CU_J_SYS=("Cornell Java SYS" "src/main/java/edu/cornell/kfs/sys" "" "false" "false")
-CU_X_SYS=("Cornell XML SYS" "src/main/resources/edu/cornell/kfs/sys" "" "false" "false")
-K_J_COA=("KualiCo base overlay COA" "src/main/java/org/kuali/kfs/coa" "../financials/kfs-core/" "false" "true")
-K_J_FP=("KualiCo base overlay FP" "src/main/java/org/kuali/kfs/fp" "../financials/kfs-core/" "false" "true")
-K_J_GL=("KualiCo base overlay GL" "src/main/java/org/kuali/kfs/gl" "../financials/kfs-core/" "false" "true")
-K_J_KIM=("KualiCo base overlay KIM" "src/main/java/org/kuali/kfs/kim" "../financials/kfs-core/" "false" "true")
-K_J_KNS=("KualiCo base overlay KNS" "src/main/java/org/kuali/kfs/kns" "../financials/kfs-core/" "false" "true")
-K_J_PDP=("KualiCo base overlay PDP" "src/main/java/org/kuali/kfs/pdp" "../financials/kfs-core/" "false" "true")
-K_J_SEC=("KualiCo base overlay SEC" "src/main/java/org/kuali/kfs/sec" "../financials/kfs-core/" "false" "true")
-K_J_VND=("KualiCo base overlay VND" "src/main/java/org/kuali/kfs/vnd" "../financials/kfs-core/" "false" "true")
-K_J_AR=("KualiCo base overlay AR" "src/main/java/org/kuali/kfs/module/ar" "../financials/kfs-ar/" "false" "true")
-K_J_CAM=("KualiCo base overlay CAM" "src/main/java/org/kuali/kfs/module/cam" "../financials/kfs-cam/" "false" "true")
-K_J_CG=("KualiCo base overlay CG" "src/main/java/org/kuali/kfs/module/cg" "../financials/kfs-cg/" "false" "true")
-K_J_LD=("KualiCo base overlay LD" "src/main/java/org/kuali/kfs/module/ld" "../financials/kfs-ld/" "false" "true")
-K_J_PURAP=("KualiCo base overlay PURAP" "src/main/java/org/kuali/kfs/module/purap" "../financials/kfs-purap/" "false" "true")
+kualicoBase="../financials"
+kualicoBaseCore="$kualicoBase/kfs-core/"
+cuKfsOrgBase="src/main/java/org/kuali/kfs"
+cuKfsJavaEduBase="src/main/java/edu/cornell/kfs"
+cuKfsXmlEduBase="src/main/resources/edu/cornell/kfs"
+
+K_J_SYS=("KualiCo base overlay SYS" "$cuKfsOrgBase/sys" "$kualicoBaseCore" "false" "false")
+CU_J_SYS=("Cornell Java SYS" "$cuKfsJavaEduBase/sys" "" "false" "false")
+CU_X_SYS=("Cornell XML SYS" "$cuKfsXmlEduBase/sys" "" "false" "false")
+
+K_J_COA=("KualiCo base overlay COA" "$cuKfsOrgBase/coa" "$kualicoBaseCore" "false" "false")
+CU_J_COA=("Cornell Java COA" "$cuKfsJavaEduBase/coa" "" "false" "false")
+CU_X_COA=("Cornell XML COA" "$cuKfsXmlEduBase/coa" "" "false" "false")
+
+K_J_FP=("KualiCo base overlay FP" "$cuKfsOrgBase/fp" "$kualicoBaseCore" "false" "false")
+CU_J_FP=("Cornell Java FP" "$cuKfsJavaEduBase/fp" "" "false" "false")
+CU_X_FP=("Cornell XML FP" "$cuKfsXmlEduBase/fp" "" "false" "false")
+
+K_J_GL=("KualiCo base overlay GL" "$cuKfsOrgBase/gl" "$kualicoBaseCore" "false" "false")
+CU_J_GL=("Cornell Java GL" "$cuKfsJavaEduBase/gl" "" "false" "false")
+CU_X_GL=("Cornell XML GL" "$cuKfsXmlEduBase/gl" "" "false" "false")
+
+K_J_KIM=("KualiCo base overlay KIM" "$cuKfsOrgBase/kim" "$kualicoBaseCore" "false" "false")
+CU_J_KIM=("Cornell Java KIM" "$cuKfsJavaEduBase/kim" "" "false" "false")
+CU_X_KIM=("Cornell XML KIM" "$cuKfsXmlEduBase/kim" "" "false" "false")
+
+K_J_KNS=("KualiCo base overlay KNS" "$cuKfsOrgBase/kns" "$kualicoBaseCore" "false" "false")
+CU_J_KNS=("Cornell Java KNS" "$cuKfsJavaEduBase/kns" "" "false" "false")
+CU_X_KNS=("Cornell XML KNS" "$cuKfsXmlEduBase/kns" "" "false" "false")
+
+K_J_PDP=("KualiCo base overlay PDP" "$cuKfsOrgBase/pdp" "$kualicoBaseCore" "false" "false")
+CU_J_PDP=("Cornell Java PDP" "$cuKfsJavaEduBase/pdp" "" "false" "false")
+CU_X_PDP=("Cornell XML PDP" "$cuKfsXmlEduBase/pdp" "" "false" "false")
+
+K_J_SEC=("KualiCo base overlay SEC" "$cuKfsOrgBase/sec" "$kualicoBaseCore" "false" "false")
+CU_J_SEC=("Cornell Java SEC" "$cuKfsJavaEduBase/sec" "" "false" "false")
+CU_X_SEC=("Cornell XML SEC" "$cuKfsXmlEduBase/sec" "" "false" "false")
+
+K_J_VND=("KualiCo base overlay VND" "$cuKfsOrgBase/vnd" "$kualicoBaseCore" "false" "false")
+CU_J_VND=("Cornell Java VND" "$cuKfsJavaEduBase/vnd" "" "false" "false")
+CU_X_VND=("Cornell XML VND" "$cuKfsXmlEduBase/vnd" "" "false" "false")
+
+K_J_AR=("KualiCo base overlay AR" "$cuKfsOrgBase/module/ar" "$kualicoBase/kfs-ar/" "false" "false")
+CU_J_AR=("Cornell Java AR" "$cuKfsJavaEduBase/module/ar" "" "false" "false")
+CU_X_AR=("Cornell XML AR" "$cuKfsXmlEduBase/module/ar" "" "false" "false")
+
+K_J_CAM=("KualiCo base overlay CAM" "$cuKfsOrgBase/module/cam" "$kualicoBase/kfs-cam/" "false" "false")
+CU_J_CAM=("Cornell Java CAM" "$cuKfsJavaEduBase/module/cam" "" "false" "false")
+CU_X_CAM=("Cornell XML CAM" "$cuKfsXmlEduBase/module/cam" "" "false" "false")
+
+K_J_CG=("KualiCo base overlay CG" "$cuKfsOrgBase/module/cg" "$kualicoBase/kfs-cg/" "false" "false")
+CU_J_CG=("Cornell Java CG" "$cuKfsJavaEduBase/module/cg" "" "false" "false")
+CU_X_CG=("Cornell XML CG" "$cuKfsXmlEduBase/module/cg" "" "false" "false")
+
+K_J_LD=("KualiCo base overlay LD" "$cuKfsOrgBase/module/ld" "$kualicoBase/kfs-ld/" "false" "false")
+CU_J_LD=("Cornell Java LD" "$cuKfsJavaEduBase/module/ld" "" "false" "false")
+CU_X_LD=("Cornell XML LD" "$cuKfsXmlEduBase/module/ld" "" "false" "false")
+
+K_J_PURAP=("KualiCo base overlay PURAP" "$cuKfsOrgBase/module/purap" "$kualicoBase/kfs-purap/" "false" "false")
+CU_J_PURAP=("Cornell Java PURAP" "$cuKfsJavaEduBase/module/purap" "" "false" "false")
+CU_X_PURAP=("Cornell XML PURAP" "$cuKfsXmlEduBase/module/purap" "" "false" "false")
 
 modules=(
   K_J_SYS[@]
   CU_J_SYS[@]
   CU_X_SYS[@]
   K_J_COA[@]
+  CU_J_COA[@]
+  CU_X_COA[@]
   K_J_FP[@]
+  CU_J_FP[@]
+  CU_X_FP[@]
   K_J_GL[@]
+  CU_J_GL[@]
+  CU_X_GL[@]
   K_J_KIM[@]
+  CU_J_KIM[@]
+  CU_X_KIM[@]
   K_J_KNS[@]
+  CU_J_KNS[@]
+  CU_X_KNS[@]
   K_J_PDP[@]
+  CU_J_PDP[@]
+  CU_X_PDP[@]
   K_J_SEC[@]
+  CU_J_SEC[@]
+  CU_X_SEC[@]
   K_J_VND[@]
+  CU_J_VND[@]
+  CU_X_VND[@]
   K_J_AR[@]
+  CU_J_AR[@]
+  CU_X_AR[@]
   K_J_CAM[@]
+  CU_J_CAM[@]
+  CU_X_CAM[@]
   K_J_CG[@]
+  CU_J_CG[@]
+  CU_X_CG[@]
   K_J_LD[@]
+  CU_J_LD[@]
+  CU_X_LD[@]
   K_J_PURAP[@]
+  CU_J_PURAP[@]
+  CU_X_PURAP[@]
 )
 
 reportFile="../technicalReport.txt"
@@ -91,4 +162,5 @@ done
 
 cd ..
 
-# rm -rf cu-kfs
+rm -rf cu-kfs
+rm -rf financials
